a4e083dce63c8109cc094cdddc1baee37df7f44d
JackyChan-cs <47530923+JackyChan-cs@users.noreply.github.com>
2020-09-08 11:40:23 -0400

    Add files via upload

--- SpotifyTopCharts/src/Main.java
+++ SpotifyTopCharts/src/Main.java
@@ -1,12 +1,10 @@
-import java.io.File;
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
-import java.io.FileNotFoundException;
+import java.io.*;
 import java.util.Arrays;
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.io.File;
+import java.util.*;
+
 /**
  * Read in spotify top charts csv into a 2d array and formatted the output
  *
@@ -15,27 +13,27 @@ import java.io.File;
  */
 public class Main {
     public static void main(String[] args) throws FileNotFoundException {
-
+        PrintStream output = new PrintStream("Artists-WeekOfMMDDYYYY.txt");
         String fileName = "regional-global-daily-lastest.csv";
-        File file = new File("C:\\Users\\somet\\IdeaProjects\\Spotify top charts\\src\\regional-global-daily-latest.csv"); //Create filepath for csv file
+        File file = new File("C:\\Users\\somet\\IdeaProjects\\Spotify top charts\\src\\regional-global-daily-latest.csv");
         String line = "";
-        String[][] list = new String[201][5];       //creates 2d array
+        String[][] list = new String[201][5];
 
 
         try {
-            BufferedReader br = new BufferedReader(new FileReader(file));  //creates object that reads the data in the csv file
-            int i = 0;                                      //counter to make output more dynamic 
-            while ((line = br.readLine()) != null) {        //loop reads through the csv line by line until the end
+            BufferedReader br = new BufferedReader(new FileReader(file));
+            int i = 0;
+            while ((line = br.readLine()) != null) {
 
-                String[] lineData = line.split(",");          //creates a string array that splits the csv data by commans
-                list[i]= lineData;                            //converts the data in the lineData array into the 2d array.  
-    
+                String[] values = line.split(",");
+                list[i]= values;
+
                 System.out.printf("");
-                System.out.printf("%-13s%-42s%-35s%-35s%-35s%n",list[i][0], list[i][1] ,list[i][2] ,list[i][3] ,list[1][4]);  //formats the output statement 
+                output.printf("%-13s%-42s%-35s%-35s%-35s%n",list[i][0], list[i][1] ,list[i][2] ,list[i][3] ,list[i][4]);
 
-                i++;            
+                i++;
             }
-            }catch(FileNotFoundException e){        //exception handling
+            }catch(FileNotFoundException e){
                 e.printStackTrace();
             }catch(IOException e){
                 e.printStackTrace();
